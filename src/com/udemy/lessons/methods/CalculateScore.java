0933a3a7f3a585bd9cb597ba49a284d4ebbee634
oleg.jar@gmail.com <Porshetic1719>
2019-08-27 14:11:49 +0300

    CalculateScore alternative way of method

--- src/com/udemy/lessons/methods/CalculateScore.java
+++ src/com/udemy/lessons/methods/CalculateScore.java
@@ -5,13 +5,59 @@ public class CalculateScore {
     public static void main(String[] args) {
         calculateScore(true, 800, 5, 100);
         calculateScore(true, 10000, 8, 200);
+
+
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Olegs", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Bob", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Percy", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Gilbert", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Louise", highScorePosition);
+
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-            finalScore +=2000;
+            finalScore += 2000;
             System.out.println("Your final score was " + finalScore);
+            return finalScore;
         }
+        return -1;
     }
+
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into " + highScorePosition + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+/*        if (playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500) {
+            return 2;
+        } else if (playerScore >= 100) {
+            return 3;
+        }
+        return 4;*/
+
+    int position = 4;
+    if(playerScore >= 1000) {
+        position = 1;
+    } else if (playerScore >= 500) {
+        position = 2;
+    } else if (playerScore >= 100) {
+        position = 3;
+    }
+    return position;
+    }
+
 }
